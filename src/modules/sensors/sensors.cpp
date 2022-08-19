4207b920ecbc44c5b2b0bd05f45d7c8ab00b938f
bresch <brescianimathieu@gmail.com>
2022-08-19 16:34:15 +0200

    imu: depending on IMU location, publish as vehicle_imu or payload_imu
    IMU location is defined in CAL_ACCx_LOC

--- src/modules/sensors/sensors.cpp
+++ src/modules/sensors/sensors.cpp
@@ -462,7 +462,7 @@ void Sensors::InitializeVehicleIMU()
 				const bool multi_mode = (_param_sens_imu_mode.get() == 0);
 				const px4::wq_config_t &wq_config = multi_mode ? px4::ins_instance_to_wq(i) : px4::wq_configurations::INS0;
 
-				VehicleIMU *imu = new VehicleIMU(i, i, i, wq_config);
+				VehicleIMU *imu = new VehicleIMU(i, i, i, wq_config, calibration::GetCalibrationParamInt32("ACC",  "LOC", i));
 
 				if (imu != nullptr) {
 					// Start VehicleIMU instance and store
